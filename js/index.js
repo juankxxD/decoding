9d63c4fab33edc225eb85d791804d34b393093a3
juankxxd <juangamerospina@gmail.com>
2022-12-19 13:45:01 -0500

    Parte logica

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$cifrar = document.getElementById('cifrar');
+$decifrar = document.getElementById('decifrar');
+$textInput = document.getElementById('text');
+$result = document.getElementById('response');
+$copiar = document.getElementById('copiar');
+
+$cifrar.onclick = (() => {
+    let cifrado;
+    const regex = /[A-Z]|[-ÿ\u00f1\u00d1]|[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/g;
+    let workCorrect = $textInput.value.match(regex) ? false : true;
+    if(!workCorrect) alert('no se puede tener mayusculas o caracteres especiales');
+    else {
+        cifrado = $textInput.value.replaceAll('e', 'enter');
+        cifrado = cifrado.replaceAll('i', 'imes');
+        cifrado = cifrado.replaceAll('a', 'ai');
+        cifrado = cifrado.replaceAll('o', 'ober');
+        cifrado = cifrado.replaceAll('u', 'ufat');
+        $result.value = cifrado;
+    }
+});
+
+$decifrar.onclick = (() => {
+    let decifrado;
+    const regex = /[A-Z]|[-ÿ\u00f1\u00d1]|[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/g;
+    let workCorrect = $textInput.value.match(regex) ? false : true;
+    if(!workCorrect) alert('no se puede tener mayusculas o caracteres especiales');
+    else {
+        decifrado = $textInput.value.replaceAll('enter', 'e');
+        decifrado = decifrado.replaceAll('imes', 'i');
+        decifrado = decifrado.replaceAll('ai', 'a');
+        decifrado = decifrado.replaceAll('ober', 'o');
+        decifrado = decifrado.replaceAll('ufat', 'u');
+        $result.value = decifrado;
+    }
+})
+
+$copiar.onclick = (() => {
+    $result.select();
+    document.execCommand("copy");
+})
